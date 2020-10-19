86a41a9ee650d5bd0fef4261ee624d468ef97a67
Curtis Abell <curtis.abell@adelaide.edu.au>
2020-10-19 14:22:54 +1030

    About to try lsp mode

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(simpleclip yasnippet use-package babel)))
+ '(package-selected-packages '(pdf-tools auctex simpleclip yasnippet use-package babel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -12,4 +12,13 @@
  '(org-block ((t (:inherit shadow :extend t :background "gray10" :foreground "white"))))
  '(org-block-background ((t nil)))
  '(org-block-begin-line ((t (:inherit org-meta-line :foreground "gray" :underline "gray"))))
- '(org-block-end-line ((t (:inherit org-block-begin-line :overline t :underline nil)))))
+ '(org-block-end-line ((t (:inherit org-block-begin-line :overline t :underline nil))))
+ '(org-document-title ((t (:font "DejaVu Sans" :weight bold :height 1.5))))
+ '(org-level-1 ((t (:font "DejaVu Sans" :weight bold :height 1.75 :underline t :foreground "Forest Green"))))
+ '(org-level-2 ((t (:font "DejaVu Sans" :weight bold :height 1.5 :foreground "goldenrod"))))
+ '(org-level-3 ((t (:font "DejaVu Sans" :weight bold :height 1.25 :foreground "cyan"))))
+ '(org-level-4 ((t (:font "DejaVu Sans" :weight bold :height 1.1 :foreground "OrangeRed"))))
+ '(org-level-5 ((t (:font "DejaVu Sans" :weight bold :height 1.1 :foreground "Lime Green"))))
+ '(org-level-6 ((t (:font "DejaVu Sans" :weight bold :height 1.1 :foreground "steel blue"))))
+ '(org-level-7 ((t (:font "DejaVu Sans" :weight bold :height 1.1 :foreground "violet red"))))
+ '(org-level-8 ((t (:font "DejaVu Sans" :weight bold :height 1.1 :foreground "slate grey")))))
