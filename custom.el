80962768c785f163570675d021928bbdeb2c7f6a
Curtis Abell <curtisabell97@gmail.com>
2021-11-02 16:24:16 +1030

    update idk

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("4255def6ba48bc1002f553aef704e232e1ee8acf7a5686fc622c522e51fde490" "13929e755ce635ddc6b9ddabe8cf47fa7df911c77c4bc49c7fe6ef4f62bafdf3" "f2e602c34d5f64e07e6f8dcfd567e4cf0d361a67358d5ee81ae6f8e6d09a21e2" "81d0a223115aae4b724cf00e9afa8ee7e4c1a3314ec30c9f39a848deaa56b593" "b3342270fa855ec71909b4eadad31b12846c29deb045d88e4d6b83f1dcda3737" "fc083ecfcb889a70f09dc3a9f1e373fd3898da6867d8e3d5c8989b5e48c79c3f" "d91b9d8ea7693191f80cc39eac721e8e9d5e66008342829999cc8f1967fc86f7" "99729d58f0330fa07898f928dd386d24085084cddceb1104f8f32178e8ac299f" "a911d9f4789cc77e7ad851fbca0d6000d39fc621502e8b9501990b21f7009c37" "2a82c990bd34f09deecd88239f2a751345a43c659d8eb3ba1ad85f62eba02319" "120ef7818cdf58c1e628a294df4764c8b427845e894b5d13bea52d724f942666" "1b85d7c615e7f7f852defd5fe54540a93fd93cf41badf4166f25ff62ddc8f56f" "d0ac211e62849d3aed426287b1dfa42bce3d15beccf9e6a7b523bac226101d51" default))
  '(helm-completion-style 'emacs)
- '(package-selected-packages '(simpleclip yasnippet use-package babel)))
+ '(package-selected-packages '(company-reftex simpleclip yasnippet use-package babel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
